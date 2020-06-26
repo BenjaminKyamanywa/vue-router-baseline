7f4c0d59f842ec8915880a4ce80e3983de99181b
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-06-27 02:33:51 +0300

    edits added bootstrap-vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,17 @@
 <template>
-  
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="primary">
+    <b-navbar-brand href="#">Home</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Blog</b-nav-item>
+        <b-nav-item href="#">Services</b-nav-item>
+        <b-nav-item href="#">Contact</b-nav-item>
+      </b-navbar-nav>
+  </div>
 </template>
 
 <script>

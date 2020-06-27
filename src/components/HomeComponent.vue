82aa4926f5e0ec239072b694021286d62eeae4da
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-06-27 03:02:00 +0300

    edits home component

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Home</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Blog</b-nav-item>
+        <b-nav-item href="#">Services</b-nav-item>
+        <b-nav-item href="#">Contact</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeComponent',
+
+    data: () => ({
+      
+    }),
+  }
+</script>

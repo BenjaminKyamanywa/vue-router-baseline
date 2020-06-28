19f706071497a47161b25f88bdfe1f6c6088a326
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-06-28 17:48:46 +0300

    added router link for home view

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">Home</b-navbar-brand>
+    <b-navbar-brand to="/">Home</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 

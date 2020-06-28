7377ba32c4e485af72b1bf3e2ace795ddab9ce19
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-06-28 17:40:25 +0300

    updated navbar to top os screen on home view

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/"></router-link>
-      <router-link to="/about"></router-link>
+    <div>
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
@@ -17,16 +15,4 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
